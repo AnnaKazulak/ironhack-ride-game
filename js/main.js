8abe8e7d54d17346a92fa8b29cb29c0b3b7a3d16
Anna Kazulak <anna.kazulak@gmail.com>
2023-08-17 17:31:50 +0200

    refactore code, add sound effect by shooting, improve collision detection function add welcome page WIP

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ class Player {
     this.positionX = 50 - this.width / 2;
     this.positionY = 0;
     this.airplaneImg = null;
-    this.obstacleCount = 0;
 
     this.createAirplane();
   }
@@ -44,70 +43,6 @@ class Player {
     this.positionY++;
     this.airplaneImg.style.bottom = this.positionY + "vw";
   }
-  // count points for removed obstacle and display it
-  increaseObstacleCount() {
-    this.obstacleCount++;
-    const pointsTable = document.getElementById("points-table");
-    pointsTable.textContent = this.obstacleCount;
-  }
-
-  shoot() {
-    const bullet = document.createElement("div");
-    bullet.className = "bullet";
-
-    // Set the initial position of the bullet
-    bullet.style.left = this.positionX + this.width / 2 + "vw";
-    bullet.style.bottom = this.positionY + this.height + "vh";
-
-    // Append the bullet to the DOM
-    const board = document.getElementById("board");
-    board.appendChild(bullet);
-
-    // Move the bullet upwards with an interval
-    const bulletInterval = setInterval(() => {
-      const bulletTop = parseFloat(bullet.style.bottom);
-      bullet.style.bottom = bulletTop + 1 + "vh";
-
-      // Check for collisions with obstacles
-      const obstacles = document.querySelectorAll(".obstacle");
-
-      obstacles.forEach((obstacle) => {
-        if (checkCollision(bullet, obstacle)) {
-          clearInterval(bulletInterval);
-          board.removeChild(bullet);
-          board.removeChild(obstacle);
-          this.increaseObstacleCount();
-
-          // Create explosion SVG
-          const explosion = document.createElement("img");
-          explosion.setAttribute("src", "./images/explosion.svg");
-          explosion.className = "explosion";
-
-          // Set the position of the explosion
-          const explosionLeft = parseFloat(obstacle.style.left);
-          const explosionTop = parseFloat(obstacle.style.bottom);
-          explosion.style.left = explosionLeft + "vw";
-          explosion.style.bottom = explosionTop + "vh";
-
-          // Append the explosion to the DOM
-          board.appendChild(explosion);
-
-          // count explosion
-
-          // Remove the explosion after a delay
-          setTimeout(() => {
-            board.removeChild(explosion);
-          }, 1000);
-        }
-      });
-
-      // Check if the bullet is out of the screen
-      if (bulletTop >= 100) {
-        clearInterval(bulletInterval);
-        board.removeChild(bullet);
-      }
-    }, 10);
-  }
 }
 
 class Obstacle {
@@ -131,7 +66,7 @@ class Obstacle {
     this.domElement.setAttribute("src", imageAttribut);
 
     // set id
-    this.domElement.className = "obstacle tree";
+    this.domElement.className = "obstacle";
     this.domElement.style.width = this.width + "vw";
     this.domElement.style.height = this.height + "vh";
     this.domElement.style.left = this.positionX + "vw";
@@ -159,176 +94,38 @@ class Obstacle {
   }
 }
 
-const player = new Player();
+class Bullet {
+  constructor(positionX, positionY) {
+    this.width = 0.2;
+    this.height = 2;
+    this.positionX = positionX;
+    this.positionY = positionY;
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft" && player.positionX >= 0) {
-    player.moveLeft();
-    petrolBoard.decreaseFuel();
-  } else if (
-    event.key === "ArrowRight" &&
-    player.positionX < 100 - player.width
-  ) {
-    player.moveRight();
-    petrolBoard.decreaseFuel();
-  } else if (
-    event.key === "ArrowUp" &&
-    player.positionY <= 100 - player.width
-  ) {
-    player.moveUp();
-    petrolBoard.decreaseFuel();
-  } else if (event.key === "ArrowDown" && player.positionY > 0) {
-    // buck: player can go to the northpool
-    player.moveDown();
-    petrolBoard.decreaseFuel();
+    this.bullet = null;
+    this.speedY = 1;
+
+    this.createDomElement();
   }
-});
+  createDomElement() {
+    this.bullet = document.createElement("div");
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === " ") {
-    player.shoot();
+    // Set the initial position of the bullet
+    this.bullet.className = "bullet";
+    this.bullet.style.width = this.width + "vw";
+    this.bullet.style.height = this.height + "vh";
+    this.bullet.style.left = this.positionX + "vw";
+    this.bullet.style.bottom = this.positionY + "vh";
+
+    // Append the bullet to the DOM
+    const board = document.getElementById("board");
+    board.appendChild(this.bullet);
   }
-});
 
-const dangerousObstaclesArray = []; // submarine, shippr
-const notDangerousObstaclesArrayLeft = []; // trees, buildings
-const notDangerousObstaclesArrayRight = []; // trees, buildingsl
-const dangerousObstaclesArrayRight = []; // fight-jet, helicopter
-const fuelArray = []; // fuel
-
-// create dangerous obstacles
-setInterval(() => {
-  const positionXRiver = Math.random() * (60 - 20) + 20;
-  const imageAttributArray = [
-    "./images/submarine-right.svg",
-    "./images/ship-right.svg",
-  ];
-  const imageAttribut =
-    imageAttributArray[Math.floor(Math.random() * imageAttributArray.length)];
-
-  const newObstacleRiver = new Obstacle(positionXRiver, imageAttribut);
-  dangerousObstaclesArray.push(newObstacleRiver);
-}, 5000);
-
-//create NOT dangerous obstacles Left
-setInterval(() => {
-  const positionXLeft = Math.floor(Math.random() * 16);
-
-  const imageAttributArray = ["./images/tree.svg", "./images/trees.svg"];
-  const imageAttribut =
-    imageAttributArray[Math.floor(Math.random() * imageAttributArray.length)];
-  const newObstacleLeft = new Obstacle(positionXLeft, imageAttribut);
-  notDangerousObstaclesArrayLeft.push(newObstacleLeft);
-}, 5500);
-
-//create NOT dangerous obstacles Right
-setInterval(() => {
-  const positionXRight = Math.floor(Math.random() * (95 - 80) + 80);
-
-  const imageAttributArray = ["./images/tree.svg", "./images/trees.svg"];
-  const imageAttribut =
-    imageAttributArray[Math.floor(Math.random() * imageAttributArray.length)];
-  const newObstacleRighr = new Obstacle(positionXRight, imageAttribut);
-  notDangerousObstaclesArrayRight.push(newObstacleRighr);
-}, 4500);
-
-// create fuel
-setInterval(() => {
-  const positionXRiver = Math.floor(Math.random() * (60 - 20) + 20);
-  const imageAttributFuel = "./images/gas-station.svg";
-  const newObstacleRiver = new Obstacle(positionXRiver, imageAttributFuel);
-  fuelArray.push(newObstacleRiver);
-}, 10000);
-
-// move dangerous obstacles River
-setInterval(() => {
-  dangerousObstaclesArray.forEach((obstacleInstance) => {
-    if (obstacleInstance.positionY > -10) {
-      obstacleInstance.moveDown();
-    }
-
-    if (obstacleInstance.positionY < 50 && obstacleInstance.positionX > 50) {
-      obstacleInstance.moveRight();
-      if (obstacleInstance.positionX > 80 - obstacleInstance.width) {
-        obstacleInstance.moveLeft();
-      }
-    }
-
-    if (obstacleInstance.positionY < 50 && obstacleInstance.positionX < 50) {
-      obstacleInstance.moveLeft();
-      if (obstacleInstance.positionX < 20) {
-        obstacleInstance.moveRight();
-      }
-    }
-
-    if (obstacleInstance.positionY < 0) {
-      obstacleInstance.domElement.remove(); //remove from the dom
-      dangerousObstaclesArray.shift(); // remove from the array
-    }
-
-    if (
-      player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
-      player.positionX + player.width > obstacleInstance.positionX &&
-      player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
-      player.positionY + player.height > obstacleInstance.positionY
-    ) {
-      // Collision detected!
-      console.log("game over! ");
-      // location.href = "./gameover.html";
-    }
-  });
-}, 100);
-
-// move NOT dangerous obstacles Left
-setInterval(() => {
-  let points = 0;
-  notDangerousObstaclesArrayLeft.forEach((obstacleInstance) => {
-    if (obstacleInstance.positionY > -10) {
-      obstacleInstance.moveDown();
-    }
-    if (obstacleInstance.positionY < 0) {
-      obstacleInstance.domElement.remove(); //remove from the dom
-      notDangerousObstaclesArrayLeft.shift(); // remove from the array
-    }
-  });
-}, 100);
-
-// move NOT dangerous obstacles Right
-setInterval(() => {
-  let points = 0;
-  notDangerousObstaclesArrayRight.forEach((obstacleInstance) => {
-    if (obstacleInstance.positionY > -10) {
-      obstacleInstance.moveDown();
-    }
-    if (obstacleInstance.positionY < 0) {
-      obstacleInstance.domElement.remove(); //remove from the dom
-      notDangerousObstaclesArrayRight.shift(); // remove from the array
-    }
-  });
-}, 100);
-let fuelLevel = 0;
-
-// move fuel
-setInterval(() => {
-  fuelArray.forEach((obstacleInstance) => {
-    if (obstacleInstance.positionY > -10) {
-      obstacleInstance.moveDown();
-    }
-    if (obstacleInstance.positionY < 0) {
-      obstacleInstance.domElement.remove(); //remove from the dom
-      fuelArray.shift(); // remove from the array
-    }
-    if (
-      player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
-      player.positionX + player.width > obstacleInstance.positionX &&
-      player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
-      player.positionY + player.height > obstacleInstance.positionY
-    ) {
-      // Collision detected!
-      petrolBoard.increaseFuel();
-    }
-  });
-}, 100);
+  moveUp() {
+    this.positionY++;
+    this.bullet.style.bottom = this.positionY + "vw"; // hard code BUG!
+  }
+}
 
 class Fuel {
   constructor() {
@@ -370,16 +167,313 @@ class Fuel {
   }
 }
 
-const petrolBoard = new Fuel(fuelLevel);
+const dangerousObstaclesArray = []; // submarine, shippr
+const notDangerousObstaclesArrayLeft = []; // trees
+const notDangerousObstaclesArrayRight = []; // trees
+const dangerousObstaclesArrayRight = []; // fight-jet, helicopter
+const fuelArray = []; // fuel
+const bulletsArray = []; // bulets
+let fuelLevel = 0;
+let obstacleCount = 0;
 
-// Function to check collision between two elements
-function checkCollision(elem1, elem2) {
-  const rect1 = elem1.getBoundingClientRect();
-  const rect2 = elem2.getBoundingClientRect();
-  return (
-    rect1.left < rect2.right &&
-    rect1.right > rect2.left &&
-    rect1.top < rect2.bottom &&
-    rect1.bottom > rect2.top
-  );
+const dangerousObstacleInterval = 5000;
+const leftObstacleInterval = 5500;
+const rightObstacleInterval = 4500;
+const fuelInterval = 10000;
+
+const leftPositionXRange = [0, 16]; // hart  code
+const rightPositionXRange = [80, 95]; // hart  code
+const fuelPositionXRange = [20, 60]; // hart  code
+const dangerousPositionXRange = [20, 60]; // hart  code
+
+const notDangerousImageAttributArray = [
+  "./images/tree.svg",
+  "./images/trees.svg",
+];
+const dangerousImageAttributArray = [
+  "./images/submarine-right.svg",
+  "./images/ship-right.svg",
+];
+const fuelImageAttribut = "./images/gas-station.svg";
+
+// create obstacles
+function createObstacles(
+  interval,
+  positionXRange,
+  imageAttributArray,
+  targetArray
+) {
+  setInterval(() => {
+    const positionX = Math.floor(
+      Math.random() * (positionXRange[1] - positionXRange[0]) +
+        positionXRange[0]
+    );
+    const imageAttribut =
+      imageAttributArray[Math.floor(Math.random() * imageAttributArray.length)];
+    const newObstacle = new Obstacle(positionX, imageAttribut);
+    targetArray.push(newObstacle);
+  }, interval);
 }
+
+createObstacles(
+  dangerousObstacleInterval,
+  dangerousPositionXRange,
+  dangerousImageAttributArray,
+  dangerousObstaclesArray
+);
+createObstacles(
+  leftObstacleInterval,
+  leftPositionXRange,
+  notDangerousImageAttributArray,
+  notDangerousObstaclesArrayLeft
+);
+createObstacles(
+  rightObstacleInterval,
+  rightPositionXRange,
+  notDangerousImageAttributArray,
+  notDangerousObstaclesArrayRight
+);
+createObstacles(
+  fuelInterval,
+  fuelPositionXRange,
+  [fuelImageAttribut],
+  fuelArray
+);
+
+// create bulletsArray
+function createBullestArray() {
+  setInterval(() => {
+    bulletsArray.forEach((bullet) => {
+      bullet.moveUp();
+      if (bullet.positionY >= 100) {
+        bullet.bullet.remove();
+        bulletsArray.shift();
+      }
+    });
+  }, 10);
+}
+
+// count points for removed obstacle and display it
+function increaseObstacleCount() {
+  obstacleCount++;
+  console.log(obstacleCount);
+  const pointsTable = document.getElementById("points-table");
+  pointsTable.textContent = obstacleCount;
+}
+
+function moveObstacles(obstacleArray, collisionCallback) {
+  setInterval(() => {
+    obstacleArray.forEach((obstacleInstance, obstIndex, obstArr) => {
+      if (obstacleInstance.positionY > -10) {
+        obstacleInstance.moveDown();
+      }
+      if (obstacleInstance.positionY < 0) {
+        obstacleInstance.domElement.remove(); // remove from the dom
+        obstArr.splice(obstIndex, 1); // remove from the array
+      }
+      if (collisionCallback) {
+        collisionCallback(obstacleInstance, obstIndex, obstArr);
+      }
+    });
+  }, 100);
+}
+
+// collision with a bullet
+function collisionWithBullets(obstacleInstance, obstIndex, obstArr) {
+  if (bulletsArray.length === 0) {
+    return;
+  }
+
+  bulletsArray.forEach((bulletInstance, i) => {
+    if (
+      bulletInstance.positionX <
+        obstacleInstance.positionX + obstacleInstance.width &&
+      bulletInstance.positionX + bulletInstance.width >
+        obstacleInstance.positionX &&
+      bulletInstance.positionY <
+        obstacleInstance.positionY + obstacleInstance.height &&
+      bulletInstance.positionY + bulletInstance.height >
+        obstacleInstance.positionY
+    ) {
+      bulletsArray.splice(i, 1); // remove from array
+      bulletInstance.bullet.remove(); // remove from DOM
+      obstArr.splice(obstIndex, 1);
+      obstacleInstance.domElement.remove();
+      makeExplosion(obstacleInstance);
+      increaseObstacleCount();
+    }
+  });
+}
+
+// move NOT dangerous obstacles
+function moveNotDangerousObstacles(obstaclesArray) {
+  setInterval(() => {
+    obstaclesArray.forEach((obstacleInstance) => {
+      if (obstacleInstance.positionY > -10) {
+        obstacleInstance.moveDown();
+      }
+      if (obstacleInstance.positionY < 0) {
+        obstacleInstance.domElement.remove(); //remove from the dom
+        obstaclesArray.shift(); // remove from the array
+      }
+    });
+  }, 100);
+}
+
+// move dangerous obstacles River
+function moveDangerousObstacleRiver() {
+  setInterval(() => {
+    dangerousObstaclesArray.forEach(
+      (obstacleInstance, obstacleInstanceIndex, arr) => {
+        if (obstacleInstance.positionY > -10) {
+          obstacleInstance.moveDown();
+        }
+
+        if (
+          obstacleInstance.positionY < 50 &&
+          obstacleInstance.positionX > 50
+        ) {
+          obstacleInstance.moveRight();
+          if (obstacleInstance.positionX > 80 - obstacleInstance.width) {
+            obstacleInstance.moveLeft();
+          }
+        }
+
+        if (
+          obstacleInstance.positionY < 50 &&
+          obstacleInstance.positionX < 50
+        ) {
+          obstacleInstance.moveLeft();
+          if (obstacleInstance.positionX < 20) {
+            obstacleInstance.moveRight();
+          }
+        }
+
+        if (obstacleInstance.positionY < 0) {
+          obstacleInstance.domElement.remove(); //remove from the dom
+          dangerousObstaclesArray.shift(); // remove from the array
+        }
+        checkCollision(player, dangerousObstaclesArray);
+        collisionWithBullets(obstacleInstance, obstacleInstanceIndex, arr);
+      }
+    );
+  }, 100);
+}
+
+// move fuel
+
+function moveFuel() {
+  setInterval(() => {
+    fuelArray.forEach((obstacleInstance) => {
+      if (obstacleInstance.positionY > -10) {
+        obstacleInstance.moveDown();
+      }
+      if (obstacleInstance.positionY < 0) {
+        obstacleInstance.domElement.remove(); //remove from the dom
+        fuelArray.shift(); // remove from the array
+      }
+      if (
+        player.positionX <
+          obstacleInstance.positionX + obstacleInstance.width &&
+        player.positionX + player.width > obstacleInstance.positionX &&
+        player.positionY <
+          obstacleInstance.positionY + obstacleInstance.height &&
+        player.positionY + player.height > obstacleInstance.positionY
+      ) {
+        // Collision detected!
+        petrolBoard.increaseFuel();
+      }
+    });
+  }, 100);
+}
+
+// Colision  detection
+function checkCollision(player, obstaclesArr) {
+  obstaclesArr.forEach((obstacleInstance, i, arr) => {
+    if (
+      player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+      player.positionX + player.width > obstacleInstance.positionX &&
+      player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+      player.positionY + player.height > obstacleInstance.positionY
+    ) {
+      arr.splice(i, 1);
+      console.log("game over!");
+      location.href = "./gameover.html";
+      obstacleInstance.domElement.remove();
+      playGameOverSound();
+    }
+  });
+}
+// explosion
+function makeExplosion(obstacle) {
+  // Create explosion SVG
+  const explosion = document.createElement("img");
+  explosion.setAttribute("src", "./images/explosion.svg");
+  explosion.className = "explosion";
+
+  // Set the position of the explosion
+  const explosionLeft = obstacle.positionX;
+  const explosionTop = obstacle.positionY;
+  explosion.style.left = explosionLeft + "vw";
+  explosion.style.bottom = explosionTop + "vh";
+
+  // Append the explosion to the DOM
+  board.appendChild(explosion);
+
+  // Remove the explosion after a delay
+  setTimeout(() => {
+    board.removeChild(explosion);
+  }, 1000);
+}
+
+// moving player
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft" && player.positionX >= 0) {
+    player.moveLeft();
+    petrolBoard.decreaseFuel();
+  } else if (
+    event.key === "ArrowRight" &&
+    player.positionX < 100 - player.width
+  ) {
+    player.moveRight();
+    petrolBoard.decreaseFuel();
+  } else if (
+    event.key === "ArrowUp" &&
+    player.positionY <= 100 - player.width
+  ) {
+    player.moveUp();
+    petrolBoard.decreaseFuel();
+  } else if (event.key === "ArrowDown" && player.positionY > 0) {
+    // buck: player can go to the northpool
+    player.moveDown();
+    petrolBoard.decreaseFuel();
+  }
+});
+
+// shooting booltes
+document.addEventListener("keydown", (event) => {
+  if (event.key === " ") {
+    const xPos = player.positionX + player.width / 2;
+    const yPOs = player.positionY;
+    const newBullet = new Bullet(xPos, yPOs);
+    bulletsArray.push(newBullet);
+    playShootSound();
+  }
+});
+
+function playShootSound() {
+  let audio = new Audio("../audio/shoot-laser.mp3");
+  audio.play();
+}
+
+function playGameOverSound() {
+  let audio = new Audio("../audio/game-over-sound.mp3");
+  audio.play();
+}
+moveNotDangerousObstacles(notDangerousObstaclesArrayRight);
+moveNotDangerousObstacles(notDangerousObstaclesArrayLeft);
+moveDangerousObstacleRiver(dangerousObstaclesArray);
+moveFuel(fuelArray);
+createBullestArray();
+const player = new Player();
+const petrolBoard = new Fuel(fuelLevel);
